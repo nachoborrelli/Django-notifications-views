e4b3664c0537fec4f0b4ad7318cc1728c9fdab2a
Juan Borrelli <nacho2911@hotmail.com>
2024-08-13 15:06:44 -0300

    [ VERSION ] Update

--- django_notifications_views/__version__.py
+++ django_notifications_views/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'Django-notifications-views'
 __description__ = 'Django-notifications-views is an extension for Django-notifications-hq that provides a viewset for the notifications.'
 __url__ = 'https://github.com/nachoborrelli/Django-notifications-views'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = '@nachoborrelli https://github.com/nachoborrelli'
 __author_email__ = 'nacho2911@hotmail.com'
 __license__ = 'MIT'
