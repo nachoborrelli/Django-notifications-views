b3c6964d5120568ef8490cf95ba5468cb4036a60
405298-Gattas <agusgattasrp@gmail.com>
2024-08-19 15:29:40 -0300

    [fix] delete_old_notifications

--- django_notifications_views/management/commands/delete_old_notifications.py
+++ django_notifications_views/management/commands/delete_old_notifications.py
@@ -4,22 +4,25 @@ from django.conf import settings
 from datetime import timedelta
 from swapper import load_model
 
-# Cargar el modelo Notification usando load_model
 Notification = load_model('notifications', 'Notification')
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        # Verificar si la opción de autoeliminar notificaciones está habilitada en la configuración
         if not settings.DJANGO_NOTIFICATIONS_VIEWS.get('AUTO_DELETE_NOTIFICATIONS', False):
             self.stdout.write("Auto delete notifications is disabled.")
             return
 
-        # Calcular la fecha umbral (notificaciones de más de 30 días)
         threshold_date = timezone.now() - timedelta(days=30)
-        
-        # Eliminar notificaciones antiguas
-        deleted, _ = Notification.objects.filter(timestamp__lte=threshold_date).delete()
-        
-        # Mostrar el número de notificaciones eliminadas
-        self.stdout.write(f"Deleted {deleted} old notifications.")
+
+        # Ver cuántas notificaciones cumplen con el filtro
+        notifications_to_delete = Notification.objects.filter(timestamp__lte=threshold_date)
+        total_notifications = notifications_to_delete.count()
+
+        self.stdout.write(f"Found {total_notifications} notifications to delete.")
+
+        if total_notifications > 0:
+            deleted, _ = notifications_to_delete.delete()
+            self.stdout.write(f"Deleted {deleted} old notifications.")
+        else:
+            self.stdout.write("No notifications to delete.")
