69dcbe3b36d0de899d8f9eea5c2f0aa7f5bd1447
405298-Gattas <agusgattasrp@gmail.com>
2024-08-22 20:09:49 -0300

    [fix] new fix

--- django_notifications_views/cron_config.py
+++ django_notifications_views/cron_config.py
@@ -5,14 +5,14 @@ import os
 COMMAND = "python manage.py delete_old_notifications"
 
 # Obtener la configuración del cron desde settings del proyecto
-CRON_SCHEDULE = getattr(settings, 'DJANGO_NOTIFICATIONS_CRON_SCHEDULE', '0 0 * * *')  # Default: todos los días a las 00:00
+CRON_SCHEDULE = getattr(settings, 'DJANGO_NOTIFICATIONS_CRON_SCHEDULE', '* * * * *')  # Default: todos los días a las 00:00
 
 # Crear la línea de cron
 CRON_JOB = f"{CRON_SCHEDULE} {COMMAND}"
 
 # Guardar el cron en un archivo temporal que se pueda utilizar en la configuración del contenedor
 def write_cron_job():
-    cron_file_path = os.path.join("django_base", "notifications_cron")
+    cron_file_path = os.path.join(settings.BASE_DIR, "notifications_cron")
     with open(cron_file_path, "w") as cron_file:
         cron_file.write(f"{CRON_JOB}\n")
     return cron_file_path
